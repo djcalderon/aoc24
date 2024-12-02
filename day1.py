859930d527c50c6dd98400c4852de05187d25547
Derek Calderon <derek@derekcalderon.com>
2024-12-02 18:58:41 -0500

    Day 1 star 2

--- day1.py
+++ day1.py
@@ -1,3 +1,5 @@
+### part one
+
 # initialize two lists to hold location IDs
 list1 = list(())
 list2 = list(())
@@ -18,10 +20,24 @@ list1.sort()
 list2.sort()
 
 # sum up the distances between each pair of list items
-sumOfDistances = 0
+totalDistance = 0
 for i in range(len(list1)):
     distance = abs(list1[i] - list2[i])
-    sumOfDistances += distance
+    totalDistance += distance
 
 # print the answer
-print(sumOfDistances)
+print("Total distance between lists:", totalDistance)
+
+### part two
+
+# calculate similarity scores
+totalSimilarity = 0
+for i in range(len(list1)):
+    hitCount = 0
+    for j in range(len(list2)):
+        if list2[j] == list1[i]:
+            hitCount += 1
+    totalSimilarity += list1[i] * hitCount
+
+# print the answer
+print("Similarity score between lists:", totalSimilarity)
