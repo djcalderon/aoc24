8a27a0632041e98400a337b9ade7b874a46d73c0
Derek Calderon <derek@derekcalderon.com>
2024-12-03 19:49:42 -0500

    Day 3 star 2

--- day3.py
+++ day3.py
@@ -1,14 +1,21 @@
 import re
 
 sumOfProducts = 0
+mulEnabled = True
 
 for line in open("inputs/day3.txt", "r"):
-    # find all valid mul instructions within the input line
-    mulInstructions = re.findall("mul\\(\\d{1,3},\\d{1,3}\\)", line)
+    # find all valid instructions within the input line
+    mulInstructions = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", line)
     for i in mulInstructions:
-        # isolate the two factors for this mul operation
-        factors = re.findall("\\d{1,3}", i)
-        # multiply factors and add product to running sum
-        sumOfProducts += int(factors[0]) * int(factors[1])
+        # determine what type of instruction this is
+        if "do()" in i:
+            mulEnabled = True
+        elif "don't()" in i:
+            mulEnabled = False
+        elif "mul(" in i and mulEnabled == True:
+            # isolate the two factors for this mul operation
+            factors = re.findall("\\d{1,3}", i)
+            # multiply factors and add product to running sum
+            sumOfProducts += int(factors[0]) * int(factors[1])
 
 print("Sum of all products:", sumOfProducts)
